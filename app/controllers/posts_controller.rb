534ade9e956a4f9cc43c9e088be0508868cf1cb4
Victor Otieno <victorotieno598@gmail.com>
2021-06-17 01:00:29 +0300

    Add create  method and new view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,4 +6,24 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params.merge(user_id: current_user.id))
+
+    respond_to do |format|
+      if @post.save
+        format.html { redirect_to @post, notice: 'Post was successfully created.' }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
 end
